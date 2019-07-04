4f2e9529b3787fc1259768d601b27d1528e6e79e
Ken Nichol <ken.nichol@live.ca>
2019-07-04 16:00:14 -0700

    redefine app as a class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { React, Component } from 'react';
+import React,{ Component } from 'react';
 
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
-      <div className="App">
-        <h1>Music App</h1>
-      </div>
+    <div className="App">
+      <h1>Music App</h1>
+    </div>
     );
   }
 }
